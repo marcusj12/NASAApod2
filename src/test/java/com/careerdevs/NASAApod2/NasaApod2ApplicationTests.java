b6877bca77301e5965b6824aaa4be45f12cdc6e7
marcusj12 <marcus.a.jennings12@gmail.com>
2022-05-03 18:58:33 -0400

    NasaApodAPI2

---
+++ src/test/java/com/careerdevs/NASAApod2/NasaApod2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.NASAApod2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApod2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
